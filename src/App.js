4d6c6544a153a4408e8449738bd73c36ded56d05
Quan Pham <mockanterr@gmail.com>
2023-01-12 19:49:17 +0700

    made select into a form component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,57 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [sens, setSens] = useState("");
+  const [input, setInput] = useState("val");
+  const [output, setOutput] = useState("cm");
+  useEffect(() => {
+    console.log("input:", input);
+    console.log("output:", output);
+  });
+
   function sensFilter(event) {
     setSens(event.target.value);
   }
+
+  function handleInputChange(e) {
+    setInput(e.target.value);
+  }
+
+  function handleOutputChange(e) {
+    setOutput(e.target.value);
+  }
+
   return (
     <div className="container">
-      <form>
-        <div className="io">
-          <label>
-            input:
-            <select>
-              <option value="val">Valorant</option>
-              <option value="apex">Apex Legends</option>
-              <option value="cm">cm/360</option>
-            </select>
-          </label>
-          <label>
-            output:
-            <select>
-              <option value="val">Valorant</option>
-              <option value="apex">Apex Legends</option>
-              <option value="cm">cm/360</option>
-            </select>
-          </label>
-        </div>
-        <div className="sens">
-          <label>
-            sensitivity:
-            <input type="number" value={sens} onChange={sensFilter} />
-          </label>
-          <label>
-            dpi:
-            <input type="number" />
-          </label>
-        </div>
-      </form>
+      <div className="io">
+        <label>
+          input:
+          <select value={input} onChange={handleInputChange}>
+            <option value="val">Valorant</option>
+            <option value="apex">Apex Legends</option>
+            <option value="cm">cm/360</option>
+          </select>
+        </label>
+        <label>
+          output:
+          <select value={output} onChange={handleOutputChange}>
+            <option value="val">Valorant</option>
+            <option value="apex">Apex Legends</option>
+            <option value="cm">cm/360</option>
+          </select>
+        </label>
+      </div>
+      <div className="sens">
+        <label>
+          sensitivity:
+          <input type="number" value={sens} onChange={sensFilter} />
+        </label>
+        <label>
+          dpi:
+          <input type="number" />
+        </label>
+      </div>
       <p className="converted">converted sens: {sens}</p>
     </div>
   );
